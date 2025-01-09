b39d81f24d683595bc3018a634ec971fbd52acbf
wcsomega <wcsomega@gmail.com>
2025-01-08 23:51:49 -0500

    Created folder structure for the project

--- Entities/sign/sign.gd
+++ Entities/sign/sign.gd
@@ -1,11 +1,13 @@
 extends StaticBody2D
 
+@export var text: String = "Test"
+
 var textbox_scene: PackedScene
 
 func _ready() -> void:
 	textbox_scene = preload("res://textbox.tscn")
 
 func on_interact():
-	print("read sign")
+	print("read sign: %s" % text)
 	var tb = textbox_scene.instantiate()
 	add_child(tb)
