9f045a49df940ab5e03048b162d60bc2287b6287
wcsomega <wcsomega@gmail.com>
2025-01-11 01:24:32 -0500

    Fix broken house entrance and exit

--- Levels/house_ext_test/house_ext_test.gd
+++ Levels/house_ext_test/house_ext_test.gd
@@ -3,4 +3,4 @@ extends Node2D
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		print("entered house")
-		get_tree().change_scene_to_file("res://house_int_test.tscn")
+		get_tree().change_scene_to_file("res://Levels/house_int_test/house_int_test.tscn")
