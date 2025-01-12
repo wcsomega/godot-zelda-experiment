e0b4e46bfeab5f078e6a94be44e5a1a8c791ab23
wcsomega <wcsomega@gmail.com>
2025-01-11 20:40:39 -0500

    Change health bar to dynamically create ui hearts Change health bar to smoothly interpolate when health changes

--- HUD/HealthBar/ui_heart.gd
+++ HUD/HealthBar/ui_heart.gd
@@ -1,4 +1,4 @@
-extends TextureRect
+extends Sprite2D
 
 var amount = 4:
 	get:
@@ -9,6 +9,5 @@ var amount = 4:
 			amount = 4
 		if amount < 0:
 			amount = 0
-		var tex = texture as AtlasTexture
 		var x = amount * 8
-		tex.region = Rect2(x, 16, 8, 8)
+		region_rect = Rect2(x, 16, 8, 8)
--- Levels/house_ext_test/hud.gd
+++ Levels/house_ext_test/hud.gd
@@ -1,7 +1,33 @@
 extends CanvasLayer
 
+var target_health = 4
+var shown_health: int
+var max_hearts = 15
+var ui_hearts = []
+
+func _ready() -> void:
+	for i in max_hearts:
+		var ui_heart = preload("res://HUD/HealthBar/ui_heart.tscn").instantiate()
+		ui_hearts.push_back(ui_heart)
+		var x = i % 10
+		var y = i / 10
+		ui_heart.position = Vector2(x*8, y*8)
+		add_child(ui_heart)
+	shown_health = target_health
+	change_health(target_health)
+
+func change_health(new_health: int) -> void:
+	target_health = clamp(new_health, 0, max_hearts * 4)
+
 func _process(delta: float) -> void:
+	if target_health > shown_health:
+		shown_health -= 1
+	if target_health < shown_health:
+		shown_health -= 1
+	for i in ui_hearts.size():
+		var ui_heart = ui_hearts[i]
+		ui_heart.amount = shown_health - (i * 4)
 	if Input.is_action_just_pressed("hp_up"):
-		$UIHeart.amount += 1
+		change_health(target_health + 16)
 	if Input.is_action_just_pressed("hp_down"):
-		$UIHeart.amount -= 1
+		change_health(target_health - 16)
