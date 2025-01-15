05584333b36e04b7e6f767b4798a0eb48acff006
wcsomega <wcsomega@gmail.com>
2025-01-15 02:21:40 -0500

    Fix warning message when changing scenes during collision callback

--- Levels/house_int_test/house_int_test.gd
+++ Levels/house_int_test/house_int_test.gd
@@ -3,4 +3,4 @@ extends Node2D
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		get_tree().change_scene_to_file("res://Levels/house_ext_test/house_ext_test.tscn")
+		get_tree().call_deferred("change_scene_to_file", "res://Levels/house_ext_test/house_ext_test.tscn")
